9d128788acac15d5410857ee87a83ce45c0760ff
Vladimir Kolomeytsev <vsvetliy@gmail.com>
2021-11-03 04:05:32 +0700

    125254

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,6 +1,6 @@
 module.exports = (err, req, res, next) => {
   if (!err.statusCode || err.statusCode === 500) {
-    res.status(500).send({ message: 'Ошибка сервера' });
+    res.status(500).send({ message: err.message });
   } else {
     res.status(err.statusCode).send({ message: err.message });
   }
